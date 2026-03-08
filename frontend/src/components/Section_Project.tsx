02a4c66ccaacc317d4dea2186b11b41dd95696c2
UmbertoCarbone <umberto.carb.dev@gmail.com>
2026-03-08 17:29:08 +0100

    try depoly

--- frontend/src/components/Section_Project.tsx
+++ frontend/src/components/Section_Project.tsx
@@ -35,7 +35,9 @@ export default function Section_Project() {
           My <span className="text-gradient-violet">Projects</span>
         </h2>
       </div>
+   
 
+   
       {/* Grid Progetti */}
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-16 px-4 md:px-8">
         {progetti.map((progetto) => (
